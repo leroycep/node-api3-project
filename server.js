b33c33d3fd0145d980301d408617cbc2a9a964cd
LeRoyce Pearson <leroycepearson@geemili.xyz>
2020-04-08 14:34:02 -0600

    Implement validateUserId and GET `/api/users/:user_id`

--- server.js
+++ server.js
@@ -1,13 +1,17 @@
 const express = require('express');
 
+const userRouter = require("./users/userRouter.js");
+
 const server = express();
+
+//custom middleware
+server.use(logger);
 
 server.get('/', (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`);
 });
 
-//custom middleware
-server.use(logger);
+server.use('/api/users', userRouter);
 
 function logger(req, res, next) {
     console.log(`[${new Date().toISOString()}] ${req.method} ${req.originalUrl}`);
--- users/userRouter.js
+++ users/userRouter.js
@@ -1,39 +1,55 @@
-const express = require('express');
+const express = require("express");
+
+const db = require("./userDb.js");
 
 const router = express.Router();
 
-router.post('/', (req, res) => {
-  // do your magic!
+router.post("/", (req, res) => {
+  res.status(500).json({ message: "not yet implemented" });
 });
 
-router.post('/:id/posts', (req, res) => {
-  // do your magic!
+router.post("/:id/posts", (req, res) => {
+  res.status(500).json({ message: "not yet implemented" });
 });
 
-router.get('/', (req, res) => {
-  // do your magic!
+router.get("/", (req, res) => {
+  res.status(500).json({ message: "not yet implemented" });
 });
 
-router.get('/:id', (req, res) => {
-  // do your magic!
+router.get("/:user_id", validateUserId, (req, res) => {
+  res.status(200).json(req.user);
 });
 
-router.get('/:id/posts', (req, res) => {
-  // do your magic!
+router.get("/:user_id/posts", (req, res) => {
+  res.status(500).json({ message: "not yet implemented" });
 });
 
-router.delete('/:id', (req, res) => {
-  // do your magic!
+router.delete("/:user_id", (req, res) => {
+  res.status(500).json({ message: "not yet implemented" });
 });
 
-router.put('/:id', (req, res) => {
-  // do your magic!
+router.put("/:user_id", (req, res) => {
+  res.status(500).json({ message: "not yet implemented" });
 });
 
 //custom middleware
 
 function validateUserId(req, res, next) {
-  // do your magic!
+  db.getById(req.params.user_id)
+    .then((user) => {
+      if (user) {
+        req.user = user;
+        next();
+      } else {
+        res.status(404).json({ error: "No user with the specified id" });
+      }
+    })
+    .catch((err) => {
+      console.log(
+        `[${new Date().toISOString()}] error retrieving user from db: ${err}`
+      );
+      res.status(500).json({ error: "Failed to retrieve user data" });
+    });
 }
 
 function validateUser(req, res, next) {
